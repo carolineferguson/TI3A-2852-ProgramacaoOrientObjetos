bd7ab94523c69ee5b73d44a0569ae062136d6d67
Caroline Ferguson <carolineferguson1@outlook.com>
2020-08-13 09:55:47 -0300

    primeira aula

---
+++ Aula_01/src/Aluno.java
@@ -0,0 +1,16 @@
+
+public class Aluno {
+	//atributo ou propriedade ou variavel de instancia --> variaveis 
+	int ra;
+	String nome,curso;
+	double nota1,nota2;
+	//metodo para calcular e retornar a média do aluno
+	public double calcularMedia() {
+		return (nota1+nota2)/2;
+	}
+	
+
+}
+    
+
+
---
+++ Aula_01/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[]srgs) {
+		//gerando um objeto --> variavel --> tipo(obrigatoriamente uma classe)
+		Aluno a = new Aluno();
+		a.nome = "Humberto";
+		a.nota1 = 8.5;
+		a.nota2 = 4.25;
+		System.out.println(a.calcularMedia());
+	    
+	}
+
+}
