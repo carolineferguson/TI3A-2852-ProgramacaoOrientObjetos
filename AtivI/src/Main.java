086e047416082b9aa48d2b012a2b1011f1e0d851
Caroline Ferguson <carolineferguson1@outlook.com>
2020-09-24 02:38:33 -0300

    ajustes

--- AtivI/src/Main.java
+++ AtivI/src/Main.java
@@ -1,62 +1,103 @@
+package Main;
 import javax.swing.JOptionPane;
 import br.espm.bilhete.*;
 import br.espm.tipo.*;
 import br.espm.usuario.Usuario;
-import java.util.Random;
 import br.espm.admin.Admin;
 public class Main {
+	
 
 	public static void main(String[] args) {
-		Usuario u1 = new Usuario("Du","5342132",TipoDeUsuario.NORMAL); //Fazer vetor de usuarios
-		Usuario u2 = new Usuario("Tais","4322324",TipoDeUsuario.ESTUDANTE);
+		Usuario[] usuario = new Usuario[2];
+		BilheteUnico[] bilhetes = new BilheteUnico[2];
 		Admin admin = new Admin();
 		final String senha = "admin"; 
+		
+		//Preencher usuarios para teste
+		for(int i=0;i<usuario.length;i++) {
+			String nome  = JOptionPane.showInputDialog("Nome");
+			String cpf  = JOptionPane.showInputDialog("Cpf");
+			String tipo = JOptionPane.showInputDialog("Tipo:");
+			if(tipo.equals("ESTUDANTE")) {
+				usuario[i]  = new Usuario(nome,cpf,TipoDeUsuario.ESTUDANTE);
+			}
+			else if(tipo.equals("PROFESSOR")){
+				usuario[i]  = new Usuario(nome,cpf,TipoDeUsuario.PROFESSOR);
+			}
+			else if(tipo.equals("NORMAL")) {
+				usuario[i]  = new Usuario(nome,cpf,TipoDeUsuario.NORMAL);
+			}
+			else {
+				JOptionPane.showMessageDialog(null,"Tipo de usuario inexistente");
+			}
+			
+			}
+		
+		for(int i=0;i<usuario.length;i++) {
+			bilhetes[i] = admin.emitirBilhete(usuario[i]);
+		}
+		
+	
 		String aux = JOptionPane.showInputDialog("Senha ou CPF ou Sair"); //primeira janela grafica
+		
+		BilheteUnico bilhete_aux = null ;         
+		for(int i = 0;i<usuario.length;i++) {	
+			if(usuario[i].getCpf().equals(aux)) { //pegar o usuario dentro do vetor pelo cpf
+				if(usuario[i].equals(bilhetes[i].getUsuario())) { //pegar o bilhete unico pelo usuario
+					bilhete_aux = bilhetes[i]; 
+				}
+			}
+			
+		}
+		
+		
 		//se for o admin:
-		if(aux.equals("admin")) { //MUDAR
+		if(aux.equals(senha)) { //MUDAR
 			String var = "1. Emitir bilhete" + "\n" + "2.Imprimir bilhete" + "\n" + "3.Consultar bilhete" + "\n" + "4.Sair";
 			String op = JOptionPane.showInputDialog(var); //segunda janela grafica
 				if(op.equals("1")) {
-					BilheteUnico b1 = admin.emitirBilhete(u1);
+					int id = Integer.parseInt(JOptionPane.showInputDialog("Qual id do usuario?")); //O Id do usuario é a posição dele no vetor
+					admin.emitirBilhete(usuario[id]);
 				}
 				else if(op.equals("2")) {
-					System.out.println(admin.imprimirBilhete());
+					System.out.println(admin.imprimirBilhete(bilhetes));
 				}
 				else if(op.equals("3")) {
-					admin.consultarBilhete(u1.getCpf());
+					admin.consultarBilhete(bilhete_aux);
 				}
 				
 				else if(op.equals("4")) { //MUDAR
 				
 				}
+		}
+				
 		//se for a opção sair		
 		else if(aux.equals("Sair")) {
 			
 		}
 		//se for o usuario
-		else { //não esta entrando neste else(mudar para else if e aplicar regex ou alguma outra validação)
-			Usuario usuario;                   //MUDAR=>pegar o usuario dentro do vetor pelo pdf
-				if(u1.getCpf().equals(aux)) {
-					usuario = u1;
-				}
-				else {
-					usuario = u2;
-				}
-			BilheteUnico b2 = admin.emitirBilhete(usuario);//MUDAR => pegar o bilhete unico do usuario
+		else { 
+		
+			
 			String var2 = "1. Carregar bilhete" +"\n" + "2.Passar na catraca" + "\n" + "3.Consultar saldo" + "\n" + "4.Sair";
 			String op2 = JOptionPane.showInputDialog(var2); //segunda janela grafica
 				if(op2.equals("1")) {
 					double valor = Double.parseDouble(JOptionPane.showInputDialog("Valor:"));
-					b2.carregarBilhete(valor);
+					bilhete_aux.carregarBilhete(valor);
 				}
 				else if(op2.equals("2")) {
-					b2.passarNaCatraca();
+					bilhete_aux.passarNaCatraca();
 				}
 				else if(op2.equals("3")) {
-					b2.getDados();
+					bilhete_aux.getDados();
 				}
 		}
+		
 	}
+	
+	
+}
+	
 
-}
-}
+
+
--- AtivI/src/br/espm/admin/Admin.java
+++ AtivI/src/br/espm/admin/Admin.java
@@ -2,36 +2,34 @@ package br.espm.admin;
 import br.espm.bilhete.BilheteUnico;
 import br.espm.tipo.TipoDeUsuario;
 import br.espm.usuario.Usuario;
-
 import java.util.Random;
+import Main.Main;
 
 
 public class Admin {
 	Random random = new Random();
-	Usuario u1 = new Usuario("Du","5342132",TipoDeUsuario.NORMAL); //VETOR
-	Usuario u2 = new Usuario("Tais","4322324",TipoDeUsuario.ESTUDANTE);
 	
 	public Admin() {
 		
 	}
 	
-	public BilheteUnico emitirBilhete(Usuario usuario) { //criar um vetor de bilhetes
+	public BilheteUnico emitirBilhete(Usuario usuario) { 
 		int max = 9999;									//loop com condição para que o numero do bilhete não seja igual a um ja existente dentro do vetor
 		int min = 1000;
 		int numero = random.nextInt((max-min)+1)+min;
 		return new BilheteUnico(numero,usuario,0);
 	}
 	
-	public String imprimirBilhete() {
-		return u1 +"\n"+ u2; // trazer todo o vetor criado no main
+	public String imprimirBilhete(BilheteUnico bilhete[]) {
+		String aux = "";
+		for(int i = 0;i<2;i++) {
+		aux = bilhete[i].getDados() + " "+aux;
+		}
+		return aux;
 	}
 	
-	public Usuario consultarBilhete(String cpf) {
-		if(u1.getCpf().equals(cpf)) { // loop dentro do vetor para encontrar o usuario pelo cpf 
-			return u1;
-		}
-		
-		return u2;
+	public String consultarBilhete(BilheteUnico bilhete) {
+		return bilhete.getDados();
 	}
 
 }
